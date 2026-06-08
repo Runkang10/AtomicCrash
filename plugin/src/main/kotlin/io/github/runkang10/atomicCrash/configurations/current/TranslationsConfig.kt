ccc5b7f1af14a38b96dff273c421e30e88032139
Runkang10 <chen.runkang10@gmail.com>
2026-06-08 17:07:53 +0200

    chore: Update translations - Add `crash.not-enabled` field - Rename `crash.before` field to `crash.sending` - Update translation version to 2 - Add migrations for v1 to v2

--- plugin/src/main/kotlin/io/github/runkang10/atomicCrash/configurations/current/TranslationsConfig.kt
+++ plugin/src/main/kotlin/io/github/runkang10/atomicCrash/configurations/current/TranslationsConfig.kt
@@ -1,10 +1,13 @@
 package io.github.runkang10.atomicCrash.configurations.current
 
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
+import org.spongepowered.configurate.objectmapping.meta.Comment
 import org.spongepowered.configurate.objectmapping.meta.Setting
 
 @ConfigSerializable
 data class TranslationsConfig(
+    @Comment("DO NOT TOUCH THIS.")
+    val version: Int = VERSION,
     val prefix: String = "<secondary>[<primary>Atomic<success>Crash<secondary>] <reset>",
     val crash: CrashTranslations = CrashTranslations(),
     val reload: ResultTranslations = ResultTranslations(
@@ -12,18 +15,19 @@ data class TranslationsConfig(
         "<success>Successfully reloaded configurations.",
         "<danger>Failed to reload configuration!"
     ),
-    val version: Int = VERSION
 ) {
     companion object {
-        const val VERSION = 1
+        const val VERSION = 2
     }
 
     @ConfigSerializable
     data class CrashTranslations(
+        @Setting("not-enabled")
+        val notEnabled: String = "<danger>Crash service is disabled! Is the plugin updated?",
         @Setting("self-crash")
         val selfCrash: String = "<danger>You cannot crash yourself!",
         val exempt: String = "<danger>You cannot crash <primary><target></primary>!",
-        val before: String = "<danger>Sending packets to <primary><target></primary>...",
+        val sending: String = "<warning>Sending packets to <primary><target></primary>...",
         val success: String = "<success>Successfully sent packets to <primary><target></primary>.",
         val failure: String = "<danger>Failed to send packets to <primary><target></primary>!"
     )
--- plugin/src/main/kotlin/io/github/runkang10/atomicCrash/configurations/migrations/TranslationsMigration.kt
+++ plugin/src/main/kotlin/io/github/runkang10/atomicCrash/configurations/migrations/TranslationsMigration.kt
@@ -1,17 +1,26 @@
 package io.github.runkang10.atomicCrash.configurations.migrations
 
-import io.github.runkang10.atomicCrash.configurations.current.SettingsConfig
+import io.github.runkang10.atomicCrash.configurations.current.TranslationsConfig
+import org.spongepowered.configurate.NodePath
 import org.spongepowered.configurate.transformation.ConfigurationTransformation
 
-object SettingsMigration {
+object TranslationsMigration {
     fun build() = ConfigurationTransformation.versionedBuilder()
         .versionKey("version")
-        .addVersion(1, v1Tov2())
-        .addVersion(SettingsConfig.VERSION, default())
+        .addVersion(2, v1Tov2())
         .build()
 
 
-    private fun default() = ConfigurationTransformation.empty()
+    private fun v1Tov2() = ConfigurationTransformation.builder()
+        .addAction(NodePath.path()) { _, node ->
+            val crashTranslations = TranslationsConfig.CrashTranslations()
 
-    private fun v1Tov2() = ConfigurationTransformation.empty()
+            node.node("crash", "not-enabled").set(crashTranslations.notEnabled)
+
+            val crashBeforeNode = node.node("crash", "before")
+            node.node("crash", "sending").set(crashBeforeNode?.string ?: crashTranslations.sending)
+            node.node("crash").removeChild("before")
+            null
+        }
+        .build()
 }
