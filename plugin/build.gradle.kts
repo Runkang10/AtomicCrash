8bbb31cd8c951fbaabe46268a8d864456decfc23
Runkang10 <chen.runkang10@gmail.com>
2026-06-09 15:57:20 +0200

    chore: Add kotlin coroutines as dependency and set up modrinth auto-publish

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm")
     id("com.gradleup.shadow") version "9.4.2"
     id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.modrinth.minotaur") version "2.+"
 }
 
 version = "1.1.0"
@@ -11,13 +12,16 @@ description = "A different way to surprise cheaters."
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.spongepowered:configurate-yaml:4.2.0")
+    implementation("org.spongepowered:configurate-extra-kotlin:4.2.0")
+
     implementation(project(":shared"))
     rootDir.resolve("modules").listFiles()
         .filter { it.isDirectory }
         .forEach { implementation(project(":modules:${it.name}")) }
-    implementation("org.spongepowered:configurate-yaml:4.2.0")
-    implementation("org.spongepowered:configurate-extra-kotlin:4.2.0")
 }
 
 kotlin {
@@ -46,6 +50,18 @@ tasks {
         filesMatching("paper-plugin.yml") {
             expand(props)
         }
+    }
+
+    modrinth {
+        token.set(System.getenv("MODRINTH_TOKEN"))
+
+        projectId.set("ZfLBx8El")
+        versionNumber.set(version.toString())
+        versionType.set("release")
+        uploadFile.set(shadowJar)
+        gameVersions.addAll("26.2", "26.1.2", "26.1.1", "26.1")
+        loaders.addAll("paper", "purpur", "folia")
+        changelog.set(System.getenv("CHANGELOGS"))
     }
 }
 
