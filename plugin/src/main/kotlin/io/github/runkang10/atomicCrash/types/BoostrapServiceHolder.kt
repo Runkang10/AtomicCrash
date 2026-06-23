cdfb68de9b0f8fa25e6d684a8ac80b4a1972f2af
Runkang10 <chen.runkang10@gmail.com>
2026-06-23 11:24:43 +0200

    refactor: Replace ColoredLogger with ColoredLogger from compact-mono

--- plugin/src/main/kotlin/io/github/runkang10/atomicCrash/types/BoostrapServiceHolder.kt
+++ plugin/src/main/kotlin/io/github/runkang10/atomicCrash/types/BoostrapServiceHolder.kt
@@ -2,8 +2,8 @@ package io.github.runkang10.atomicCrash.types
 
 import io.github.runkang10.atomicCrash.configurations.current.SettingsConfig
 import io.github.runkang10.atomicCrash.configurations.current.TranslationsConfig
-import io.github.runkang10.atomicCrash.services.ColoredLogger
 import io.github.runkang10.atomicCrash.services.ConfigService
+import io.github.runkang10.compactmono.services.ColoredLogger
 
 data class BoostrapServiceHolder(
     val logger: ColoredLogger,
