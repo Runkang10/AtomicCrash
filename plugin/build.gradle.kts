d1c7cf9b6322c42f4fd4c61a0d8f078b884afea7
Runkang10 <chen.runkang10@gmail.com>
2026-06-23 11:28:44 +0200

    chore: Multiple changes

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,25 +1,27 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization") version "2.4.0"
     id("com.gradleup.shadow") version "9.4.2"
     id("xyz.jpenilla.run-paper") version "3.0.2"
     id("com.modrinth.minotaur") version "2.+"
 }
 
-version = "1.1.1"
+version = "1.2.0"
 description = "A different way to surprise cheaters."
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
     compileOnly("org.spongepowered:configurate-yaml:4.2.0")
 
     implementation(kotlin("stdlib"))
+    implementation("io.github.runkang10:compact-mono:1.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     implementation("org.spongepowered:configurate-extra-kotlin:4.2.0")
 
     implementation(project(":shared"))
-    rootDir.resolve("modules").listFiles()
+    rootDir.resolve("modules")
+        .listFiles()
         .filter { it.isDirectory }
         .forEach { implementation(project(":modules:${it.name}")) }
 }
@@ -30,7 +32,7 @@ kotlin {
 
 tasks {
     runServer {
-        minecraftVersion("26.1.2")
+        minecraftVersion("26.2")
         jvmArgs("-Xms1G", "-Xmx2G")
     }
 
@@ -57,13 +59,9 @@ tasks {
         versionNumber.set(version.toString())
         versionType.set("release")
         uploadFile.set(shadowJar)
-        gameVersions.addAll("26.1.2", "26.1.1", "26.1")
+        gameVersions.addAll("26.2", "26.1.2", "26.1.1", "26.1")
         loaders.addAll("paper", "purpur", "folia")
         changelog.set(System.getenv("CHANGELOG"))
+        syncBodyFrom.set(rootProject.file("README.md").readText())
     }
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.compilerOptions {
-    freeCompilerArgs.set(listOf("-Xannotation-default-target=param-property"))
 }
