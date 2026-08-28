c95002290d010e52b8aa1da93253b36472ebaf1a
Runkang10 <chen.runkang10@gmail.com>
2026-08-28 21:25:57 +0200

    chore: set up modrinth task

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.shadow)
     alias(libs.plugins.resourceFactoryPaper)
     alias(libs.plugins.runPaper)
+    alias(libs.plugins.minotaur)
 }
 
 dependencies {
@@ -21,7 +22,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
 }
 
-val projectVersion = System.getenv("version") ?: "0.0.0-DEV"
+val projectVersion = System.getenv("version")?.removePrefix("v") ?: "0.0.0-DEV"
 
 paperPluginYaml {
     name.set(rootProject.name)
@@ -49,5 +50,20 @@ tasks {
     runServer {
         minecraftVersion("26.2")
         jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
+    }
+
+    if (System.getenv("modrinth")?.toBoolean() ?: false) modrinth {
+        token.set(System.getenv("MODRINTH_TOKEN") ?: error("MODRINTH_TOKEN is not set!"))
+        projectId.set("ZfLBx8El")
+
+        uploadFile.set(shadowJar)
+        versionType.set("release")
+        versionName.set(System.getenv("title") ?: "Untitled")
+        versionNumber.set(projectVersion)
+        changelog.set(System.getenv("changelog") ?: "_No changelog provided._")
+        loaders.addAll("paper", "purpur", "folia")
+        gameVersions.addAll("1.21.10", "1.21.11", "26.1", "26.1.1", "26.1.2", "26.2")
+
+        syncBodyFrom.set(rootProject.file("README.md").readText())
     }
 }
