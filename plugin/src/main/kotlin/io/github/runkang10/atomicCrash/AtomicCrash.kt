41c4ee64cd40b580547d6ab1bece80fa9240b452
Runkang10 <chen.runkang10@gmail.com>
2026-09-08 17:37:52 +0200

    feat: handle PlayerListener registration/unregistration and CrashRegistry clean-up

--- plugin/src/main/kotlin/io/github/runkang10/atomicCrash/AtomicCrash.kt
+++ plugin/src/main/kotlin/io/github/runkang10/atomicCrash/AtomicCrash.kt
@@ -2,17 +2,20 @@ package io.github.runkang10.atomicCrash
 
 import io.github.runkang10.atomicCrash.configurations.DefaultSettings
 import io.github.runkang10.atomicCrash.configurations.DefaultTranslations
+import io.github.runkang10.atomicCrash.listeners.PlayerListener
 import io.github.runkang10.atomicCrash.services.AtomicCrashLoader
 import io.github.runkang10.atomicCrash.services.Commands
+import io.github.runkang10.atomicCrash.services.CrashRegistry
 import io.github.runkang10.atomicCrash.services.Permissions
 import io.github.runkang10.compactmono.configuration.LoggedConfiguration
 import io.github.runkang10.compactmono.services.ColoredLogger
+import org.bukkit.event.HandlerList
 import org.bukkit.plugin.java.JavaPlugin
 
 class AtomicCrash(
     private val logger: ColoredLogger,
-    settings: LoggedConfiguration<DefaultSettings>,
-    translations: LoggedConfiguration<DefaultTranslations>,
+    private val settings: LoggedConfiguration<DefaultSettings>,
+    private val translations: LoggedConfiguration<DefaultTranslations>,
 ) : JavaPlugin() {
     private val commands = Commands(this, settings, translations, pluginMeta)
 
@@ -24,7 +27,14 @@ class AtomicCrash(
         commands.load()
     }
 
+    override fun onEnable() {
+        server.pluginManager.registerEvents(PlayerListener(settings, translations), this)
+    }
+
     override fun onDisable() {
+        HandlerList.unregisterAll(this)
+        CrashRegistry.clear()
+
         AtomicCrashLoader.unload()
     }
 }
